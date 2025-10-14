b7e3fe6906543220fc4b1a764db1629ccb638adb
XaviFelx <xavierfelix74@gmail.com>
2025-10-14 14:03:51 -0700

    main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use procfs::{CpuInfo, Current};
+use systop_rs::cpu::{print_all_cores, to_core_info};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //TODO: test main behavior for CPU
+    let cpuinfo = CpuInfo::current()?;
+
+    let mut all = Vec::new();
+    for idx in 0..cpuinfo.num_cores() {
+        if let Some(core_map) = cpuinfo.get_info(idx).as_ref() {
+            let current_core_info = to_core_info(idx, core_map);
+            all.push(current_core_info);
+        }
+    }
+    print_all_cores(&all);
+
+    Ok(())
 }
