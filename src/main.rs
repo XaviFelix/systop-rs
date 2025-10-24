a649837ec9f2a65d4e4566899c561d51161c3f56
XaviFelx <xavierfelix74@gmail.com>
2025-10-23 18:57:30 -0700

    adhoc tests - need unit tests instead

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-//TODO: Currently using raw system data in KB, need to convert data to GB or MB
+//TODO: Currently using raw system data in KB, need to convert data to GB or MB for all stats
 //TODO: Create a dedicated display decorator for the terminal
 //TODO: Use Clap and combine it with tokio for interval refreshing
 
-use procfs::{CpuInfo, Current, Meminfo};
-use systop_rs::components::{cpu::*, mem::*};
+use procfs::{CpuInfo, Current, DiskStat, Meminfo};
+use systop_rs::components::{cpu::*, disk::*, mem::*};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //TODO: test CPU behavior
@@ -17,13 +17,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     print_all_cores(&all);
+    println!("CPU DONE: \n\n");
 
     //TODO: test mem behavior
     let meminfo = Meminfo::current();
     let current_mem_info = to_mem_info(&meminfo);
     print_mem_info(&current_mem_info);
+    println!("MEM  DONE: \n\n");
 
     //TODO: test disk behaivor
+    let lines = read_lines("/proc/diskstats")?;
+    lines
+        .flatten()
+        .filter_map(|line| DiskStat::from_line(&line).ok())
+        .filter_map(|disk| build_device(disk).ok().flatten()) // drop errors here for brevity
+        .filter(|info| info.is_parent()) // only parents
+        .for_each(|info| print!("{info}")); // Display decides how to print
+    println!("DISK DONE: \n\n");
 
     Ok(())
 }
